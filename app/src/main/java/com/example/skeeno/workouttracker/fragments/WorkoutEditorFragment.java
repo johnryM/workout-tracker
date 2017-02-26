2b76710556c5a44865ee32617ab194ac6c0210a1
Johnry Mangonon <johnry.mangonon@bbc.co.uk>
2017-02-26 00:28:20 +0000

    Get Date picker frag to work

--- app/src/main/java/com/example/skeeno/workouttracker/fragments/WorkoutEditorFragment.java
+++ app/src/main/java/com/example/skeeno/workouttracker/fragments/WorkoutEditorFragment.java
@@ -1,7 +1,10 @@
 package com.example.skeeno.workouttracker.fragments;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
@@ -17,11 +20,18 @@ import android.widget.Toast;
 
 import com.example.skeeno.workouttracker.R;
 
+import java.util.GregorianCalendar;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import butterknife.Unbinder;
 
 public class WorkoutEditorFragment extends Fragment {
+
+
+    private static final String DIALOG_DATE = "DialogDate";
+    private static final int REQUEST_DATE = 0;
 
 
     @BindView(R.id.edit_text_workout_name)
@@ -32,6 +42,8 @@ public class WorkoutEditorFragment extends Fragment {
     CheckBox mCheckboxWorkoutCompleted;
 
     Unbinder mUnbinder;
+
+    private GregorianCalendar mCalendar;
 
     public WorkoutEditorFragment() {
         // Required empty public constructor
@@ -61,6 +73,10 @@ public class WorkoutEditorFragment extends Fragment {
 
         Toolbar workoutToolBar = (Toolbar) view.findViewById(R.id.toolbar);
         ((AppCompatActivity) getActivity()).setSupportActionBar(workoutToolBar);
+
+
+
+
         return view;
     }
 
@@ -68,6 +84,20 @@ public class WorkoutEditorFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         mUnbinder.unbind();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != Activity.RESULT_OK) {
+            return;
+        }
+
+        if (requestCode == REQUEST_DATE) {
+            mCalendar = (GregorianCalendar) data.getSerializableExtra(DatePickerFragment.EXTRA_DATE);
+            if (mCalendar != null) {
+                mButtonWorkoutDate.setText(mCalendar.getTime().toString());
+            }
+        }
     }
 
     @Override
@@ -90,9 +120,21 @@ public class WorkoutEditorFragment extends Fragment {
     private boolean validateWorkout() {
         String text = mEditTextWorkoutName.getText().toString();
         if (text.length() == 0) {
-            Toast.makeText(getContext(), "Its empty", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getContext(), "Workout name not entered", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if (mCalendar == null) {
+            Toast.makeText(getContext(), "Date not entered", Toast.LENGTH_SHORT).show();
             return false;
         }
         return true;
+    }
+
+    @OnClick(R.id.button_workout_date)
+    public void openCalendar() {
+        FragmentManager fm = getFragmentManager();
+        DatePickerFragment dialog = new DatePickerFragment();
+        dialog.setTargetFragment(WorkoutEditorFragment.this, REQUEST_DATE);
+        dialog.show(fm, DIALOG_DATE);
     }
 }
